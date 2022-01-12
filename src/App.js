ef2a4f58b3c5b60061d7eb451fa90b7bdf6e8a44
bohdan122 <76744691+bohdan122@users.noreply.github.com>
2022-01-12 18:07:03 +0200

    master

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {Products, Navbar} from './components';
 import { commerce } from './lib/commerce'
 
 const App = () => {
+    const [products, SetProducts] = useState([]);
+
+    const fetchProducts = async () =>{
+        const { data } = await commerce.products.list();
+
+        SetProducts(data);
+    }
+
+    useEffect(() =>{
+        fetchProducts();
+    }, []);
+
     return (
         <div>
             <Navbar/>
-            <Products />
+            <Products products = {products} />
         </div>
     );
 };
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -3,12 +3,10 @@ import Grid from '@material-ui/core/Grid';
 import useStyles from './styles-products'
 import Product from './Product/Product';
 
-const products = [
-    {id: 1, name: 'Keyboard', description: 'Membrane keyboard', price: '$20', image:'https://img.mta.ua/image/cache/data/foto/z608/60821/photos/Logitech-K400-Plus-WL-Black-1-600x600.jpg'}
-];
-
-const Products = () => {
+const Products = ({ products }) => {
     const classes = useStyles();
+
+    
     return(
     <main className={classes.content}>
         <div className={classes.toolbar} />
