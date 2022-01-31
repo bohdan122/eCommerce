e6e728711620db3a5caa1663845712cf77ccacce
bohdan122 <76744691+bohdan122@users.noreply.github.com>
2022-01-31 13:00:04 +0200

    Cart item

--- src/components/Cart/CartItem/CartItem.jsx
+++ src/components/Cart/CartItem/CartItem.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import {Typography, Button, Card, CardActions, CardContent, CardMedia} from '@material-ui/core';
 
-const CartItem = () => {
+const CartItem = ({ item }) => {
     return (
-        <div>
-            
-        </div>
+        <Card>
+            <CardMedia image = {item.media.source} alt = {item.name} className = {classes.cardmedia} />
+            <CardContent className = {classes.CardContent}>
+                <Typography variant = "h3">
+                {item.name}
+                </Typography>
+            </CardContent>
+        </Card>
     );
 };
 
