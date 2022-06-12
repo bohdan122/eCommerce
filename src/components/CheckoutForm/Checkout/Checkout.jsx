57e0137d5702fa8901452fb6e7a46a635202df3e
bohdan122 <76744691+bohdan122@users.noreply.github.com>
2022-06-12 21:26:26 +0300

    commit

---
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Paper, Stepper, Step, StepLabel, Typography, Button, Divider, CircularProgress } from '@material-ui/core'
+
+const Checkout = () => {
+  return (
+    <>
+    <div className={classes.toolbar}/>
+    <main className={calsses.layout}>
+    <Paper className={classes.paper}>
+      <Typography></Typography>
+    </Paper>
+    </main>
+    </>
+  )
+}
+
+export default Checkout
